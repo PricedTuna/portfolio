2db4554ec551708da5772cf28a76454274efbfe0
Edgar Eduardo Vega Ruiz <112290967+PricedTuna@users.noreply.github.com>
2023-06-03 23:14:16 -0700

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const githubBtn = document.getElementById("githubBtn");
+const linkedinBtn = document.getElementById("linkedinBtn");
+
+githubBtn.addEventListener("click", function() {
+  window.location.href = "https://github.com/PricedTuna"; // Replace with your GitHub profile URL
+});
+
+linkedinBtn.addEventListener("click", function() {
+  window.location.href = "https://www.linkedin.com/in/edgar-eduardo-vega-ruiz-452413252/"; // Replace with your LinkedIn profile URL
+});
